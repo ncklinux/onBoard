817cd26bcb3cad19be9cd3b13aaea9d04ba13a78
Nick <68464552+ncklinux@users.noreply.github.com>
2022-09-24 15:39:45 +0200

    feat: add header and footer components (wip)

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-navbar toggleable type="light" variant="light">
+    <b-navbar-brand href="#"><img src="~assets/svg/logo.svg" width="44" height="26"></b-navbar-brand>
+
+    <b-navbar-toggle target="navbar-toggle-collapse">
+      <template #default="{ expanded }">
+        <span v-if="expanded" class="material-icons">expand_less</span>
+        <span v-else class="material-icons">expand_more</span>
+      </template>
+    </b-navbar-toggle>
+
+    <b-collapse id="navbar-toggle-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Home</b-nav-item>
+        <b-nav-item href="#">Surfboards</b-nav-item>
+        <b-nav-item href="#">Contact</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Provide } from "nuxt-property-decorator";
+
+@Component
+export default class extends Vue {
+  public mounted() {}
+}
+</script>
+
+<style lang="scss" scoped></style>
