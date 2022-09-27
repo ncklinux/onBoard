095cee20a769bef30842495975cc1509f8928bc4
Nick <68464552+ncklinux@users.noreply.github.com>
2022-09-27 17:09:41 +0200

    feat: add meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
+      { hid: "description", name: "description", content: "onBoard e-commerce website with 3D capabilities" },
       { name: "format-detection", content: "telephone=no" },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
