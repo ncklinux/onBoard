42aa6f9d43377d413d9758975743c47c855b1bb3
Nick <68464552+ncklinux@users.noreply.github.com>
2022-12-28 21:25:57 +0100

    fix: add nuxt-i18n functionality in error layout page

--- layouts/error.vue
+++ layouts/error.vue
@@ -3,10 +3,19 @@
     <b-row class="mt-4 page">
       <b-col>
         <h6 class="errorCode" v-if="error.statusCode === 404">
-          404 Page not found
+          404
+          <span v-if="$store.state.texts.various">{{
+            $store.state.texts.various.errorNotFound[$i18n.locale]
+          }}</span>
         </h6>
         <h6 v-else>An error occurred</h6>
-        <b-button class="shadow-none" size="sm" href="/">Home page</b-button>
+        <b-button
+          class="shadow-none"
+          size="sm"
+          :to="localePath('/')"
+          v-if="$store.state.texts.menu"
+          >{{ $store.state.texts.menu[0].text[$i18n.locale] }}</b-button
+        >
       </b-col>
     </b-row>
   </b-container>
